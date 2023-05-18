0f7e20018863d9f5865bac09b015863b290c7519
Sean Cooper <seancooper@Seans-Air.lan>
2023-05-17 19:20:20 -0700

    add close functionality for about modal

--- components/AboutModal.tsx
+++ components/AboutModal.tsx
@@ -1,8 +1,16 @@
+import { useAppDispatch } from '@/store/hooks'
+import { closeModal } from '@/store/modal';
 import React from 'react'
 
 const AboutModal = () => {
+    const dispatch = useAppDispatch();
+
+    const handleClose = () => {
+        dispatch(closeModal('About'))
+    }
   return (
     <div className="flex flex-col gap-5 bg-white p-5 rounded w-2/3">
+        <button onClick={handleClose} className="text-right text-xlg">X</button>
         <h1 className="text-lg font-bold text-center">About this App</h1>
         <p>Scheduling your day can be tough. You might plan on working on a project from 9-11am, or practicing guitar from 6-7pm. But unfortunately for a lot of people, life gets in the way. Emails come in, people call, your doordash driver gets lost. Taskr is a way to make sure you get the work done no matter how hectic your schedule becomes. By setting a designated amount of time for a task, instead of just a designated time, you can make sure that progress gets done. And consistent progress leads to results. </p>
         <h1 className="text-lg font-bold text-center">How does it work?</h1>
