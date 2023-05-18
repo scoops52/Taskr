33e63fde351790371f056fd41ae678ca51173ba5
Sean Cooper <seancooper@Seans-Air.lan>
2023-05-17 20:39:32 -0700

    disabled dark mode styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import ThemeToggle from "@/components/ThemeToggle"
 import Modals from "@/components/Modals"
 import { useAppSelector } from "@/store/hooks"
 import Header from "@/components/Header"
+import Link from "next/link"
 
 export default function Home() {
 
@@ -13,6 +14,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center lg:p-24 md:p-16">
       <Providers>
         {/* <ThemeToggle /> */}
+        <Link href='https://forms.gle/s6JoeMtToqwuCjMT8' target="_blank" className="text-gray-500 underline text-sm absolute top-2 right-2 hover:text-gray-700 transition-colors">Feedback</Link>
         <Header />
         <div className="flex flex-row justify-between gap-5 items-start mt-10 w-full">
         <CreateTask />
